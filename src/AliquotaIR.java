a1f88b7ee48f28a784d2bd7c23eee93d88c11999
allrox <alexandre@allrox.com.br>
2021-09-03 20:52:29 -0300

    Implementada entrada de dados no teste de Alíquota de IR

--- src/AliquotaIR.java
+++ src/AliquotaIR.java
@@ -1,33 +1,39 @@
+import java.util.Scanner; //Importando a classe Scanner, para realizar a entrada de dados
 
 public class AliquotaIR {
 
 	public static void main(String[] args) {
 
-		System.out.println("Análise de alíquota e dedução de IR ");
-		System.out.println("");
+		Scanner ler = new Scanner(System.in); //instanciando e criando um objeto Scanner
+		double salarioInformado;
 
-		double salario = 7000.0;
+		System.out.println("Analise de alÃ­quota e deduÃ§Ã£o de IR ");
+		//System.out.println("");
+
+		System.out.println("Informe o salÃ¡rio: ");
+		salarioInformado = ler.nextDouble();
+		//double salario = 7000.0;
 		double deducao;
 
-		if (salario < 1903.98) {
+		if (salarioInformado < 1903.98) {
 			deducao = 0;
 			System.out.println("Contribuinte classificado como isento.");
-		} else if (salario <= 2826.65) {
+		} else if (salarioInformado <= 2826.65) {
 			deducao = 142.80;
-			System.out.println("Com base no salário de R$ " + salario + ","
-					+ " sua alíquota é de 7,5% e você pode deduzir R$ " + deducao + " do IR.");
-		} else if (salario <= 3751.05) {
+			System.out.println("Com base no salÃ¡rio de R$ " + salarioInformado + ","
+					+ " sua alÃ­quota Ã© de 7,5% e vocÃª pode deduzir R$ " + deducao + " do IR.");
+		} else if (salarioInformado <= 3751.05) {
 			deducao = 354.80;
-			System.out.println("Com base no salário de R$ " + salario + ","
-					+ " sua alíquota é de 15%. e você pode deduzir R$ " + deducao + " do IR.");
-		} else if (salario <= 4664.68) {
+			System.out.println("Com base no salÃ¡rio de R$ " + salarioInformado + ","
+					+ " sua alÃ­quota Ã© de 15%. e vocÃª pode deduzir R$ " + deducao + " do IR.");
+		} else if (salarioInformado <= 4664.68) {
 			deducao = 636.13;
-			System.out.println("Com base no salário de R$ " + salario + ","
-					+ " sua alíquota é de 22,5% e você pode deduzir R$ " + deducao + " do IR.");
-		} else if (salario > 4664.68) {
+			System.out.println("Com base no salÃ¡rio de R$ " + salarioInformado + ","
+					+ " sua alÃ­quota Ã© de 22,5% e vocÃª pode deduzir R$ " + deducao + " do IR.");
+		} else if (salarioInformado > 4664.68) {
 			deducao = 869.36;
-			System.out.println("Com base no salário de R$ " + salario + ","
-					+ " sua alíquota é de 27,5% e você pode deduzir R$ " + deducao + " do IR.");
+			System.out.println("Com base no salÃ¡rio de R$ " + salarioInformado + ","
+					+ " sua alÃ­quota Ã© de 27,5% e vocÃª pode deduzir R$ " + deducao + " do IR.");
 		}
 
 	}
